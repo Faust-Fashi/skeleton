74ee22dc5b105534388797ea051355e93a59c605
Faust-Fashi <847212318@qq.com>
2022-11-25 20:40:25 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,87 @@
 package hw2;
 
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] sites;
+    private boolean[][] blocked;
+    private int N;
+    private int openSize;
+    private WeightedQuickUnionUF grid;
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0)
+            throw new java.lang.IllegalArgumentException();
+        this.N = N;
+        sites = new int[N][N];
+        blocked = new boolean[N][N];
+        openSize = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                blocked[i][j] = true;
+            }
+        }
+        grid = new WeightedQuickUnionUF(xyTo1D(N, N));
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        checkBoundary(row, col, true);
+        if (!blocked[row][col])
+            return;
+        blocked[row][col] = false;
+        openSize++;
+        if (checkBoundary(row, col - 1, false) && isOpen(row, col - 1)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            openSize++;
+        }if (checkBoundary(row, col + 1, false) && isOpen(row, col + 1)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            openSize++;
+        }if (checkBoundary(row - 1, col, false) && isOpen(row - 1, col)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            openSize++;
+        }if (checkBoundary(row + 1, col, false) && isOpen(row + 1, col)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            openSize++;
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        checkBoundary(row, col, true);
+        return !blocked[row][col];
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        for (int i = 0; i < N; i++) {
+            if (isOpen(row, col) && isOpen(0, i) &&
+                grid.connected(xyTo1D(row, col), xyTo1D(0, i)) )
+                return true;
+        }
+        return false;
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+    // check if index is out of boundary
+    private boolean checkBoundary(int row, int col, boolean exception) {
+        if (row >= N || col >= N || row < 0 || col < 0)
+            if (exception)
+                throw new java.lang.IndexOutOfBoundsException();
+            else
+                return false;
+        return true;
+    }
+    private int xyTo1D(int row, int col) {
+        return row * N + col;
+    }
+    public boolean percolates() {
+        for (int i = 0; i < N; i++) {
+            if (isFull(N - 1, i))
+                return true;
+        }
+        return false;
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,38 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    double[] Prob;
+    int T;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0)
+            throw new IllegalArgumentException();
+        pf = new PercolationFactory();
+        Prob = new double[T];
+        this.T = T;
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            int randRow = StdRandom.uniform(N);
+            int randCol = StdRandom.uniform(N);
+            while (!p.percolates()) {
+                p.open(randRow, randCol);
+            }
+            Prob[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+    }
+    public double mean() {
+        return StdStats.mean(Prob);
+    }
+    public double stddev() {
+        return StdStats.stddev(Prob);
+    }
+    public double confidenceLow() {
+        return mean()-1.96*stddev()/Math.sqrt(T);
+    }
+    public double confidenceHigh(){
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
